817cbbb0f7bb92d431947682ed10c336d302d615
DeltaScripter <BioChrisD@yahoo.ca>
2013-08-05 12:14:32 -0500

    First commit

---
+++ quests/RestlessG.java
@@ -0,0 +1,220 @@
+package quests;
+
+import org.powerbot.script.methods.MethodContext;
+import org.powerbot.script.wrappers.Npc;
+import org.powerbot.script.wrappers.Tile;
+
+import quests.Vars.TeleportLode;
+import quests.Vars.TeleportType;
+
+
+public class RestlessG extends Node{
+
+
+
+	public final Tile[] to_Fath_PathFL = new Tile[] {
+			new Tile(3208,3234,0),new Tile(3213,3233,0), new Tile(3219,3225,0),
+			new Tile(3229,3218,0), new Tile(3233,3214,0), new Tile(3236,3210,0),
+			new Tile(3242,3206,0)};
+
+
+	public final Tile[] to_GhostShack = new Tile[] {
+			new Tile(3208,3234,0),new Tile(3213,3233,0), new Tile(3219,3225,0),
+			new Tile(3229,3218,0), new Tile(3233,3214,0), new Tile(3236,3210,0),
+			new Tile(3235, 3214, 0), new Tile(3236, 3205, 0),
+			new Tile(3240, 3199, 0), new Tile(3248, 3194, 0) };
+
+	public final Tile[] to_Rock = new Tile[] { 
+			new Tile(3208,3234,0),new Tile(3213,3233,0), new Tile(3219,3225,0),
+			new Tile(3229,3218,0), new Tile(3233,3214,0), new Tile(3236,3210,0),
+			new Tile(3232, 3221, 0), new Tile(3234, 3216, 0), new Tile(3234, 3211, 0), 
+			new Tile(3234, 3206, 0), new Tile(3236, 3201, 0), new Tile(3240, 3198, 0), 
+			new Tile(3243, 3194, 0), new Tile(3248, 3193, 0), new Tile(3249, 3193, 0), 
+			new Tile(3244, 3194, 0), new Tile(3243, 3194, 0), new Tile(3243, 3189, 0), 
+			new Tile(3241, 3184, 0), new Tile(3238, 3180, 0), new Tile(3239, 3175, 0), 
+			new Tile(3239, 3170, 0), new Tile(3238, 3165, 0), new Tile(3237, 3160, 0), 
+			new Tile(3236, 3155, 0), new Tile(3235, 3152, 0), new Tile(3234, 3147, 0) };
+
+	public final Tile[] pathToLonePriest = new Tile[] { 
+			new Tile(3208,3234,0),new Tile(3213,3233,0), new Tile(3219,3225,0),
+			new Tile(3229,3218,0), new Tile(3233,3214,0), new Tile(3236,3210,0),
+			new Tile(3232, 3221, 0), new Tile(3234, 3216, 0), new Tile(3235, 3211, 0), 
+			new Tile(3238, 3209, 0), new Tile(3242, 3206, 0), new Tile(3235, 3210, 0), 
+			new Tile(3236, 3205, 0), new Tile(3238, 3200, 0), new Tile(3241, 3196, 0), 
+			new Tile(3243, 3191, 0), new Tile(3241, 3186, 0), new Tile(3240, 3181, 0), 
+			new Tile(3239, 3176, 0), new Tile(3238, 3171, 0), new Tile(3239, 3166, 0), 
+			new Tile(3239, 3161, 0), new Tile(3235, 3158, 0), new Tile(3230, 3157, 0), 
+			new Tile(3225, 3157, 0), new Tile(3220, 3156, 0), new Tile(3215, 3155, 0), 
+			new Tile(3210, 3155, 0), new Tile(3206, 3153, 0) };
+	
+	final Tile[] PathToGhostFromRock = new Tile[] { new Tile(3233, 3148, 0), new Tile(3236, 3152, 0), new Tile(3240, 3155, 0), 
+			new Tile(3242, 3160, 0), new Tile(3242, 3165, 0), new Tile(3241, 3170, 0), 
+			new Tile(3240, 3175, 0), new Tile(3240, 3180, 0), new Tile(3242, 3185, 0), 
+			new Tile(3243, 3190, 0), new Tile(3246, 3195, 0), new Tile(3249, 3196, 0) };
+	
+	public final Area swampInsideHut = new Area(
+			new Tile[] { new Tile(3202, 3151, 0), new Tile(3210, 3151, 0), new Tile(3210, 3146, 0), 
+					new Tile(3201, 3146, 0) });
+	
+	public final int bankItems[] = {553,552};
+	public final int bankItemAmount[] = {1,1};
+	public Vars Vars = new Vars();
+	public Method Method = new Method(ctx);
+	
+	public RestlessG(MethodContext ctx) {
+		super(ctx);
+	}
+
+	public void execute() {
+		Method.resetTeleporting();
+		
+		DeltaQuester.numSteps = 6;
+		Method.foodSupport();
+		
+		
+		if(DeltaQuester.checkedBank &&(ctx.settings.get(2324)&0x7) !=5)
+			Method.determineBank(bankItems);
+		
+			if(DeltaQuester.checkedBank && (ctx.settings.get(2324)&0x7) !=5){
+			Method.checkBank();
+		}else
+	    if(Vars.useBank && (ctx.settings.get(2324)&0x7) !=5){
+			Method.useBank(bankItems, bankItemAmount);
+		}else
+		if((ctx.settings.get(2324)&0x7) ==5){
+			DeltaQuester.progress = 6;
+			Method.state("The Restless Ghost quest has been completed.");
+			Method.sleep(2000);
+			DeltaQuester.e = true;
+			Thread.currentThread().interrupt();
+		}else
+		if((ctx.settings.get(2324)&0x7) ==4){
+			DeltaQuester.progress = 5;
+			cs4();//go back to the ghost and place the skull in his grave
+		}else
+		if((ctx.settings.get(2324)&0x3) ==3){
+			DeltaQuester.progress = 4;
+			cs3();//Take the skull from the rock
+		}else
+		if((ctx.settings.get(2324)&0x3) ==2){
+			DeltaQuester.progress = 3;
+			cs2();//Speak to the ghost
+		}else
+		if((ctx.settings.get(2324)&0x1) ==1){
+			DeltaQuester.progress =2;
+			cs1();//Speak to the crazy priest in the swamp.
+		}else
+		if((ctx.settings.get(2324)&0x1) ==0){
+			DeltaQuester.progress = 1;
+			cs0();//Start the quest
+		}
+	}
+
+
+	private void cs4() {
+		Method m = new Method(ctx);
+		if(new Tile(3248,3193,0).distanceTo(ctx.players.local().getLocation())<5){
+			if(!ctx.objects.select().nearest().id(15061).first().isEmpty()){
+				m.useItemOn(553, 15061, "Skull");
+			}else m.interactO(2145, "Open", "Coffin");
+		}else cs2();//get to the ghost
+		
+	}
+
+	private void cs3() {//Take the skull from the rock
+		Method m = new Method(ctx);
+		if(new Tile(3236,3147,0).distanceTo(ctx.players.local().getLocation())<7){
+			Vars.DYNAMICV = false;
+			Vars.DYNAMICV2 = true;
+			m.interactO(47713, "Search", "Rock");
+		}else if(Vars.DYNAMICV){
+			m.walking(to_Rock, "Walking to ghost remains", false);
+		}else if(TeleportLode.LUMMBRIDGE.getTile().distanceTo(ctx.players.local().getLocation())<10){
+			Vars.DYNAMICV = true;
+		}else m.teleportTo(TeleportType.LUMBRIDGE.getTeleport(),TeleportType.LUMBRIDGE.getName());
+		
+	}
+	private void cs2() {//Speak to the ghost initially
+		
+		Method m = new Method(ctx);
+		String opt[] = {"Yep."};
+		if(Vars.gAmuEquip){
+			if(new Tile(3248,3193,0).distanceTo(ctx.players.local().getLocation())<5){
+				Vars.DYNAMICV = false;
+				for(Npc n : ctx.npcs.select().nearest().id(457).first()){
+					if(!m.findOption(opt))
+						if(!m.isChatting("Ghost")){
+							m.speakTo(n.getId(), "Ghost");
+							break;
+						}
+				}
+				if(!ctx.players.local().isInMotion()){
+					m.interactO(2145, "Open", "Coffin");
+				}
+				
+				
+			}else if(Vars.DYNAMICV2){
+				m.walking(PathToGhostFromRock, "Walking back to ghost", false);
+			}else if(Vars.DYNAMICV){
+				m.walking(to_GhostShack, "Walking to the ghost", false);
+			}else if(TeleportLode.LUMMBRIDGE.getTile().distanceTo(ctx.players.local().getLocation())<10){
+				Vars.DYNAMICV = true;
+			}else m.teleportTo(TeleportType.LUMBRIDGE.getTeleport(),TeleportType.LUMBRIDGE.getName());
+			
+		} else if(m.EquipmentContains(552)){
+			Vars.gAmuEquip = true;
+			Method.state("Found item in equipment" + Vars.gAmuEquip);
+		}else if(m.inventoryContains(552)){
+					m.interactInventory(552, "Wear", "Ghostspeak Amulet");
+		}
+	}
+	private void cs1() {//Speak to the crazy priest
+		
+		Method Method = new Method(ctx);
+		final String opt[] = {"A ghost","sent me to talk"};
+		if(new Tile(3206, 3153, 0).distanceTo(ctx.players.local().getLocation())<6){
+			if(swampInsideHut.contains(ctx.players.local().getLocation())){
+				Vars.DYNAMICV = false;
+				if(!Method.findOption(opt))
+					if(!Method.isChatting("Aereck")){
+						Method.speakTo(458, "Aereck");
+					}
+			}else if(Method.objIsByTile(new Tile(3207,3152,0),45539, 5)){
+				Method.interactO(45539, "Open", "Door");
+			}else ctx.movement.newTilePath(new Tile(3206,3149,0)).traverse();
+		}else if(Vars.DYNAMICV){
+			Method.walking(pathToLonePriest, "Walking to the swamp priest", false);
+		}else if(TeleportLode.LUMMBRIDGE.getTile().distanceTo(ctx.players.local().getLocation())<10){
+			Vars.DYNAMICV = true;
+		}else Method.teleportTo(TeleportType.LUMBRIDGE.getTeleport(),TeleportType.LUMBRIDGE.getName());
+	}
+		
+	private void cs0() {//Speaks to the first priest
+		final String opt[] = {"I'm looking for"};
+		Method Method = new Method(ctx);
+		
+		if(new Tile(3243,3205,0).distanceTo(ctx.players.local().getLocation())<5){
+			Vars.DYNAMICV = false;
+			if(!Method.startQuestOpen())
+			if(!Method.findOption(opt))
+			if(!Method.isChatting("Aereck")){
+				Method.speakTo(456, "Aereck");
+			}
+		}else if(Vars.DYNAMICV){
+			Method.walking(to_Fath_PathFL, "Walking to Aereck", false);
+		}else if(TeleportLode.LUMMBRIDGE.getTile().distanceTo(ctx.players.local().getLocation())<10){
+			Vars.DYNAMICV = true;
+		}else Method.teleportTo(TeleportType.LUMBRIDGE.getTeleport(),TeleportType.LUMBRIDGE.getName());
+		
+		
+	}
+
+	public boolean activate() {
+		return (DeltaQuester.scriptToStart == 1);
+	}
+
+
+
+
+
+}
---
+++ quests/TheBloodPact.java
@@ -0,0 +1,259 @@
+package quests;
+
+import org.powerbot.script.methods.MethodContext;
+import org.powerbot.script.wrappers.GameObject;
+import org.powerbot.script.wrappers.Player;
+import org.powerbot.script.wrappers.Tile;
+
+import quests.Vars.TeleportLode;
+import quests.Vars.TeleportType;
+
+public class TheBloodPact extends Node{
+
+	 public TheBloodPact(MethodContext ctx) {
+			super(ctx);
+		}
+	
+	public final Tile[] pathToStart = new Tile[] { 
+			new Tile(3210,3233,0), new Tile(3220,3227,0),new Tile(3234,3221,0),
+			new Tile(3235,3207, 0),	new Tile(3244,3198, 0)};
+	
+	public final Tile[] pathToXenia = new Tile[] {
+			new Tile(3210,3233,0), new Tile(3220,3227,0),new Tile(3234,3221,0),
+			new Tile(3232, 3222, 0), new Tile(3233, 3217, 0), new Tile(3234, 3212, 0), 
+			new Tile(3235, 3207, 0), new Tile(3237, 3202, 0), new Tile(3241, 3198, 0), 
+			new Tile(3243, 3197, 0) };
+	
+	public Tile initTile;
+	public Tile initTile2;
+	public boolean slingEquip = false;
+	public int armour[] = {15597,19830};
+	
+	public int bankItems[] = {19830,15597};
+	public int bankItemAmount[] = {1,1};
+	
+	public Method Method = new Method(ctx);
+	public void execute() {
+		DeltaQuester.numSteps = 8;
+		Method.foodSupport();
+	
+		
+		if(DeltaQuester.checkedBank)
+			Method.determineBank(bankItems);
+			if(!DeltaQuester.checkedBank&& (ctx.settings.get(2334)&0x3F)!=60){
+			Method.checkBank();
+		}else
+	    if(Vars.useBank && (ctx.settings.get(2334)&0x3F)!=60){
+			Method.useBank(bankItems, bankItemAmount);
+		}else
+		if((ctx.settings.get(2334)&0x3F)==60){
+		DeltaQuester.progress = 8;
+		Method.state("The Blood Pact quest has been completed");
+		Method.sleep(2000);
+		DeltaQuester.e = true;
+		}else
+		if((ctx.settings.get(2334)&0x3F)==55){
+			DeltaQuester.progress =7;
+		cs0();//Speak to Xenia at the end of quest
+		}else
+		if((ctx.settings.get(2334)&0x3F)==40||(ctx.settings.get(2334)&0x3F)==41||(ctx.settings.get(2334)&0x3F)==45||(ctx.settings.get(2334)&0x3F)==50){
+			DeltaQuester.progress =6;
+		cs4();//Fight Reese
+		}else
+		if((ctx.settings.get(2334)&0x3F)==35||(ctx.settings.get(2334)&0x3F)==36){
+			DeltaQuester.progress =5;
+		cs3();//Speak to the defeated Caitlin and finish off.
+		}else
+		if((ctx.settings.get(2334)&0x1F)==30){
+			DeltaQuester.progress =4;
+		cs2();//Walk to Caitlin and attack her.
+		}else
+		if((ctx.settings.get(2334)&0xF)==12|| (ctx.settings.get(2334)&0xF)==15|| (ctx.settings.get(2334)&0x1F)==25){
+			DeltaQuester.progress =3;
+		cs1();//Enter the catacombs and kill Kayle
+		}else
+		if((ctx.settings.get(2334)&0xF)==10){
+			DeltaQuester.progress =2;
+		cs1();//Enter the catacombs to trigger the cutscene
+		}else
+		if((ctx.settings.get(2334)&0xF)==0){
+			DeltaQuester.progress =1;
+		cs0();//Speak to Xenia to start the quest
+		}
+		
+	}
+	private void cs4() {
+		final String opt[] = {"die","I'm your worst","Go downstairs"};
+		if(initTile2!=null){
+			 if(new Tile(initTile2.getX()+9, initTile2.getY()-6,initTile2.getPlane()).distanceTo(ctx.players.local().getLocation())<6){
+				 if((ctx.settings.get(2334)&0x3F)==50){
+					 if(!Method.findOption(opt))
+						 if(!Method.isChatting("Ilona")){
+							 Method.npcInteract(9630, "Untie");
+						 }
+				 }else
+				 if((ctx.settings.get(2334)&0x3F)==45){//After defeating Reese finish him off
+					 if(!Method.findOption(opt))
+					 if(!Method.isChatting("Reese")){
+						 Method.speakTo(9625, "Reese");
+					 }
+				 }else
+				 if((ctx.settings.get(2334)&0x3F)==41){//Defeat Reese
+					 if(ctx.players.local().isInCombat()){
+						 Method.basicFightNPC(9624);
+					 }else Method.npcInteract(9624, "Attack");
+				 }else
+				 if(!Method.findOption(opt))
+				 if(!Method.isChatting("Reese"))
+				 Method.interactO(48793, "Open", "Tomb door");
+			}else {
+				Method.state("Walking to Reese");
+				ctx.movement.newTilePath(new Tile(initTile2.getX()+9, initTile2.getY()-6,initTile2.getPlane())).traverse();
+			}
+		}else 
+		if(!ctx.objects.select().nearest().id(48677).first().isEmpty()){
+			initTile2 = ctx.players.local().getLocation();
+		}else if(initTile!=null){
+			 if(new Tile(initTile.getX()-20, initTile.getY()+9,initTile.getPlane()).distanceTo(ctx.players.local().getLocation())<4){
+				  if(!Method.findOption(opt))
+				  if(!Method.isChatting("Xenia"))
+				 Method.interactO(48678, "Climb", "Stairs");
+				  
+			}else {
+				Method.state("Walking to stairs" + initTile);
+				Method.clickOnMap(new Tile(initTile.getX()-20, initTile.getY()+9,initTile.getPlane()));
+			}
+		}else if(!ctx.objects.select().nearest().id(48742).first().isEmpty()){//if it does exist
+			Method.sleep(1300);
+			for (GameObject marker : ctx.objects.select().nearest().id(48742).first()){//and is on screen
+				if(marker.isOnScreen())
+				initTile = ctx.players.local().getLocation();//set initial location of player
+			}
+		}else
+		if(new Tile(3246,3198,0).distanceTo(ctx.players.local().getLocation())<9){
+			Method.interactO(48797, "Climb-down", "Catacomb");
+		}else cs0();//get the player to the catacombs
+		
+	}
+	private void cs3() {
+		final String opt[] = {"Time for you to die!"};
+		if(initTile!=null){
+			
+			 if((ctx.settings.get(1261)>>18&0x3) == 2){
+				  if(new Tile(initTile.getX()-15, initTile.getY()+12,initTile.getPlane()).distanceTo(ctx.players.local().getLocation())<8){
+					  if(!Method.findOption(opt))
+					  if(!Method.isChatting("Caitlin")){
+						  Method.speakTo(9627, "Caitlin");
+					  }
+				}else {
+					Method.state("Walking to Caitlin");
+					Method.clickOnMap(new Tile(initTile.getX()-15, initTile.getY()+12,initTile.getPlane()));
+				}
+			}else
+			  if(new Tile(initTile.getX()-9, initTile.getY()+12,initTile.getPlane()).distanceTo(ctx.players.local().getLocation())<8){
+				  Method.interactO(48791, "Operate", "Door Mechanism");
+			}else {
+				Method.state("Walking to Caitlin");
+				ctx.movement.newTilePath(new Tile(initTile.getX()-9, initTile.getY()+12,initTile.getPlane())).traverse();
+			}
+			
+		}else if(!ctx.objects.select().nearest().id(48742).first().isEmpty()){//if it does exist
+			Method.sleep(1300);
+			for (GameObject marker : ctx.objects.select().nearest().id(48742).first()){//and is on screen
+				if(marker.isOnScreen())
+				initTile = ctx.players.local().getLocation();//set initial location of player
+			}
+		}else
+		if(new Tile(3246,3198,0).distanceTo(ctx.players.local().getLocation())<9){
+			Method.interactO(48797, "Climb-down", "Catacomb");
+		}else cs0();//get the player to the catacombs
+		
+	}
+	
+	private void cs2() {
+
+		if(initTile!=null){
+			
+			  if(new Tile(initTile.getX()-9, initTile.getY()+12,initTile.getPlane()).distanceTo(ctx.players.local().getLocation())<8){
+			   if(Method.isInCombat()){
+				   Method.basicFightNPC(9626);
+			   }else Method.npcInteract(9626,"Attack");
+			}else {
+				Method.state("Walking to Caitlin");
+				ctx.movement.newTilePath(new Tile(initTile.getX()-9, initTile.getY()+12,initTile.getPlane())).traverse();
+			}
+			
+		}else if(!ctx.objects.select().nearest().id(48742).first().isEmpty()){//if it does exist
+			Method.sleep(1300);
+			for (GameObject marker : ctx.objects.select().nearest().id(48742).first()){//and is on screen
+				if(marker.isOnScreen())
+				initTile = ctx.players.local().getLocation();//set initial location of player
+			}
+		}else
+			Method.interactO(48797, "Climb-down", "Catacomb");
+	
+		//}else cs0();//get the player to the catacombs
+
+	}
+
+	private void cs1() {
+		final String opt[]  ={"Yes"};
+		Player local = ctx.players.local();
+		
+		while((ctx.settings.get(1114)&0x1)==1){
+			Method.isChatting("Cutscene");
+		}
+		if(initTile!=null){
+			final Tile loc1 = new Tile(initTile.x, initTile.y+15,initTile.getPlane());
+			if(loc1.getLocation().distanceTo(ctx.players.local().getLocation())<8){
+				if((ctx.settings.get(2334)&0x1F)==25){
+					if(!Method.findOption(opt))
+					if(!Method.isChatting("Kayle")){
+						Method.speakTo(9629, "Kayle");
+					}
+				}else 
+				if(Method.isInCombat()){
+					Method.basicFightNPC(9628);
+				}else Method.npcInteract(9628, "Attack");
+			}else {
+				Method.state("Distance: " + (int)loc1.distanceTo(local.getLocation()));
+				Method.state("Walking to Kayle");
+				ctx.movement.newTilePath(loc1).traverse();
+			}
+		}else if(Method.objIsNotNull(48742)){//if it does exist
+			Method.sleep(1300);
+			for (GameObject marker : ctx.objects.select().nearest().id(48742).first()){//and is on screen
+				if(marker.isOnScreen())
+				initTile = local.getLocation();//set initial location of player
+				break;
+			}
+		}else
+		if(new Tile(3246,3198,0).distanceTo(ctx.players.local().getLocation())<9){
+			Method.interactO(48797, "Climb-down", "Catacomb");
+			Method.sleep(4000);
+		}else cs0();//get the player to the catacombs
+		
+	}
+
+	private void cs0() {
+		final String opt[] = {"I think I'll","It was nothing","I'll help you","What do you need"};
+		if(	new Tile(3243, 3197, 0).distanceTo(ctx.players.local().getLocation())<8){
+			Method.skipPics();
+			if(!Method.startQuestOpen())
+				if(!Method.findOption(opt))
+				if(!Method.isChatting("Xenia")){
+					Method.speakTo(9633, "Xenia");
+				}
+		}else if(DeltaQuester.getVars().DYNAMICV){
+			Method.walking(pathToXenia, "Walking to Xenia", false);
+		}else if(TeleportLode.LUMMBRIDGE.getTile().distanceTo(ctx.players.local().getLocation())<10){
+			DeltaQuester.getVars().DYNAMICV = true;
+		}else Method.teleportTo(TeleportType.LUMBRIDGE.getTeleport(),TeleportType.LUMBRIDGE.getName());
+		
+	}
+
+	public boolean activate() {
+		return (DeltaQuester.scriptToStart == 13);
+	}
+
+}
